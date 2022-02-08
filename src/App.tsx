f50889b8b76e6f49308ced9dd5a14037a7a0bc7f
jihkyuo <84238602+jihkyuo@users.noreply.github.com>
2022-02-08 23:27:20 +0900

    children variants & while & drag

--- src/App.tsx
+++ src/App.tsx
@@ -12,20 +12,31 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 15px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 45px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  grid-template-columns: repeat(2, 1fr);
 `;
 
-const myVars = {
-  start: { scale: 0 },
-  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 1 } },
+const boxVariants = {
+  호버얌: { scale: 1.5, rotateZ: 90 },
+  탭이얌: { scale: 1, borderRadius: "100px" },
+  드래깅: {
+    backgroundColor: "rgb(85, 239, 196)",
+    transition: { duration: 10 },
+  },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={myVars} initial="start" animate="end" />
+      <Box
+        drag
+        variants={boxVariants}
+        whileHover="호버얌"
+        whileTap="탭이얌"
+        whileDrag="드래깅"
+      />
     </Wrapper>
   );
 }
