675872ffc11f43393d0b66b20357121b4aa38af2
jihkyuo <84238602+jihkyuo@users.noreply.github.com>
2022-02-22 23:18:22 +0900

    Home / 이미지

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,11 +1,58 @@
 import { useQuery } from "react-query";
-import { getMovies } from "../api";
+import styled from "styled-components";
+import { getMovies, IGetMoviesResult } from "../api";
+import { makeImagePath } from "../utils";
+
+const Wrapper = styled.div`
+  background-color: black;
+`;
+
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Banner = styled.div<{ bgPhoto: string }>`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${(props) => props.bgPhoto});
+  background-size: cover;
+`;
+
+const Title = styled.h2`
+  margin-bottom: 20px;
+  font-size: 50px;
+`;
+
+const Overview = styled.p`
+  width: 50%;
+  font-size: 16px;
+`;
 
 function Home() {
-  const { data, isLoading } = useQuery(["movies", "nowPlaying"], getMovies);
-  console.log(data, isLoading);
+  const { data, isLoading } = useQuery<IGetMoviesResult>(
+    ["movies", "nowPlaying"],
+    getMovies
+  );
+
   return (
-    <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}>home</div>
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+        </>
+      )}
+    </Wrapper>
   );
 }
 
--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,25 @@
 const API_KEY = "c105586a40799362d1557cbaa06ec1fd";
 const BASE_PATH = "https://api.themoviedb.org/3/";
 
+interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
 export function getMovies() {
   return fetch(`${BASE_PATH}movie/now_playing?api_key=${API_KEY}`).then(
     (response) => response.json()
